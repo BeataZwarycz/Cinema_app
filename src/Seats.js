8b559ee731a39ae90ebda35987293b5fd340b55b
BeataZwarycz <bzwarycz@interia.pl>
2019-05-13 12:56:09 +0200

    Add React Component

--- src/Seats.js
+++ src/Seats.js
@@ -1,33 +1,10 @@
-<!DOCTYPE HTML>
-<html lang="pl">
+import React, { Component } from 'react';
+import './seats.css';
 
-<head>
-    <title>Cinema App</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta http-equiv="X-Ua-Compatible" content="IE=edge,chrome=1">
-    <meta name="description" content="Aplikacja kinowa stworzona jako projekt zespołowy w ramach CodersCamp." />
-    <meta name="keywords" content="app cinema coderscamp" />
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-
-    
-    <link rel="stylesheet" href="style.css">
-
-</head>
-
-<body>
-
-    <header>
-
-        <nav class="navbar">
-            <a class="logo" href="#"><i class="fa fa-film"></i> Coderscamp Cinema</a>
-        </nav>
-
-    </header>
-
-    <main>
-    <div class="hall">
+class Seats extends Component {
+    render() {
+        return(
+            <div class="hall">
         <div class="reservation-details">
             <p class="movie-name">Tytuł filmu</p>
             <p class="movie-time">Data i godzina</p>
@@ -280,11 +257,8 @@
         </div>
         </div>
     </div>
-    </main>
+        )
+    }
+}
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-     
-</body>
-</html>
+export default Seats;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Seats from './Seats';
+
+render(
+<Seats />, document.getElementById('root')
+);
+
